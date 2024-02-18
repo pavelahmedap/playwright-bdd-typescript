1fc3a8c88f49ea8a043bbbf86718313176f26ab3
Pavel Ahmed <pavelahmedap@gmail.com>
2024-02-17 19:15:29 -0500

    feat: adding node dependencies and updating the config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,6 +8,25 @@ const testDir = defineBddConfig({
 });
 
 export default defineConfig({
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  // forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 3 : 3,
+
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    // baseURL: 'http://127.0.0.1:3000',
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: "on-first-retry",
+    screenshot: "on",
+  },
+
   testDir,
   reporter: [
     ["html", { open: "never", outputFolder: "src/reports/test-results" }],
